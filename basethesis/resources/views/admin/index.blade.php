1d9751cda4c582e52376edf72260da0637826fe4
Tuan250498 <vduy4245@gmail.com>
2020-02-10 17:21:22 +0700

    update-2(V9)

--- basethesis/resources/views/admin/index.blade.php
+++ basethesis/resources/views/admin/index.blade.php
@@ -226,73 +226,13 @@
                         <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
                     </ul>
                 </li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-files-o"></i>
-                        <span>Layout Options</span>
-                        <span class="pull-right-container">
-                            <span class="label label-primary pull-right">4</span>
-                            </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="../layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-                        <li><a href="../layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-                        <li><a href="../layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-                        <li><a href="../layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="../widgets.html">
-                        <i class="fa fa-th"></i> <span>Widgets</span>
-                        <span class="pull-right-container">
-                            <small class="label pull-right bg-green">Hot</small>
-                            </span>
-                    </a>
-                </li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-pie-chart"></i>
-                        <span>Charts</span>
+                <li class="">
+                    <a href="{{route('admin.category.index')}}">
+                        <i class="fa fa-edit"></i> <span>Category</span>
                         <span class="pull-right-container">
                             <i class="fa fa-angle-left pull-right"></i>
                             </span>
                     </a>
-                    <ul class="treeview-menu">
-                        <li><a href="../charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-                        <li><a href="../charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
-                        <li><a href="../charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
-                        <li><a href="../charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
-                    </ul>
-                </li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-laptop"></i>
-                        <span>UI Elements</span>
-                        <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                            </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="../UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
-                        <li><a href="../UI/icons.html"><i class="fa fa-circle-o"></i> Icons</a></li>
-                        <li><a href="../UI/buttons.html"><i class="fa fa-circle-o"></i> Buttons</a></li>
-                        <li><a href="../UI/sliders.html"><i class="fa fa-circle-o"></i> Sliders</a></li>
-                        <li><a href="../UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
-                        <li><a href="../UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
-                    </ul>
-                </li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-edit"></i> <span>Forms</span>
-                        <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                            </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="../forms/general.html"><i class="fa fa-circle-o"></i> General Elements</a></li>
-                        <li><a href="../forms/advanced.html"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
-                        <li><a href="../forms/editors.html"><i class="fa fa-circle-o"></i> Editors</a></li>
-                    </ul>
                 </li>
                 <li class="treeview">
                     <a href="#">
--- basethesis/routes/route_admin.php
+++ basethesis/routes/route_admin.php
@@ -1,7 +1,22 @@
 <?php
-Route::group(['prefix'=>'api-admin'], function (){
+Route::group(['prefix'=>'api-admin', 'namespace'=> 'Admin'], function (){
     Route::get('/', function () {
-        return view('admin.index');
+        return view('layouts.app_master_admin');
+    });
+    /**
+     * Route danh mục sản phẩm
+     */
+    Route::group(['prefix'=>'category'], function (){
+        Route::get('', 'CategoryController@index')->name('admin.category.index');
+        Route::get('/create', 'CategoryController@create')->name('admin.category.create');
+        Route::post('/create', 'CategoryController@store');
+
+        Route::get('/update/{id}', 'CategoryController@edit')->name('admin.category.update');
+        Route::post('/update/{id}', 'CategoryController@update');
+
+        Route::get('/active/{id}', 'CategoryController@active')->name('admin.category.active');
+        Route::get('/hot/{id}', 'CategoryController@hot')->name('admin.category.hot');
+        Route::get('/delete/{id}', 'CategoryController@delete')->name('admin.category.delete');
     });
 });
 
