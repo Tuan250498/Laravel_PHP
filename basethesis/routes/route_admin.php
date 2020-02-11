babea38a8b8779708d305894605221c69ddbb21c
Tuan250498 <vduy4245@gmail.com>
2020-02-11 16:47:36 +0700

    update-ShopMobile(V14)

--- basethesis/routes/route_admin.php
+++ basethesis/routes/route_admin.php
@@ -1,12 +1,12 @@
 <?php
-Route::group(['prefix'=>'api-admin', 'namespace'=> 'Admin'], function (){
-    Route::get('/', function () {
+Route::group(['prefix' => 'api-admin', 'namespace' => 'Admin'], function () {
+    Route::get('', function () {
         return view('layouts.app_master_admin');
     });
     /**
      * Route danh mục sản phẩm
      */
-    Route::group(['prefix'=>'category'], function (){
+    Route::group(['prefix' => 'category'], function () {
         Route::get('', 'CategoryController@index')->name('admin.category.index');
         Route::get('/create', 'CategoryController@create')->name('admin.category.create');
         Route::post('/create', 'CategoryController@store');
@@ -19,4 +19,3 @@ Route::group(['prefix'=>'api-admin', 'namespace'=> 'Admin'], function (){
         Route::get('/delete/{id}', 'CategoryController@delete')->name('admin.category.delete');
     });
 });
-
--- basethesis/routes/web.php
+++ basethesis/routes/web.php
@@ -14,4 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
-include 'route_admin.php';
+
+include ('route_admin.php');
